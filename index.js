315cb59c4c53f64378ea311ed5b4865724047f85
Matias Pancorvo <matiaspancorvo@MacBook-Air-de-Matias.local>
2021-08-31 14:07:37 -0400

    finished

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+    this.items.push(item) 
+    this.items.sort(function(a,b){return a - b});
+    this.length++;
 
-  max() {}
+  }
 
-  min() {}
+  get(pos) {
+    if (pos >= 0 && pos <= this.items.length -1) {
+      return this.items[pos];
 
-  sum() {}
+    } else
+    throw new Error('OutOfBounds');
+  }
+
+  max() {
+    if (this.length === 0) {
+     throw new Error('EmptySortedList');
+
+    } else 
+    return this.items[this.items.length -1];
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+ 
+     } else 
+     return this.items[0];
+  }
+
+  sum() {
+   if  (this.length !== 0 ){
+    let total =  this.items.reduce((acc, item) => {
+       return acc + item;
+      },0);
+      return total;
+    } else {
+       return 0;
+    }
+  }
 
   avg() {}
 }
